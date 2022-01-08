cb63b1cb1989ff08bf6648cd2851c1e3506325e8
FareedaRagab <fareeeeda1@gmail.com>
2022-01-08 23:49:11 +0200

    Secant

--- src/main/java/com/example/linearequations/methods_non_linear/Secant.java
+++ src/main/java/com/example/linearequations/methods_non_linear/Secant.java
@@ -48,47 +48,40 @@ public class Secant implements Solve {
 
         int iterations = 0;
         double x2;
-        double check = expression(x0) * expression(x1);
+        do {
+            // new
+            if ((expression(x0) - expression(x1)) == 0) {
+                this.stringBuilder.append("\nThe Function Diverge!\nTry better initial guess :))");
+                return;
+            }
 
-        if (check < 0) {
-            do {
-                // new
-                if ((expression(x0) - expression(x1)) == 0) {
-                    this.stringBuilder.append("\nThe Function Diverge!\nTry better initial guess :))");
-                    return;
-                }
-
-                x2 = BigDecimal.valueOf(x1 - (expression(x1) * (x0 - x1)) / (expression(x0) - expression(x1)))
-                        .setScale(HelloController.getPre(), RoundingMode.HALF_UP).doubleValue();
-
-                // The Steps
-                this.stringBuilder.append("Iteration #").append(iterations + 1).append(": Xi-1= ")
-                        .append(x0).append(", xi= ").append(x1).append(", xi+1= ").append(x2)
-                        .append("\nRelative Approximate Error= ").append(BigDecimal.valueOf(Math.abs(x2 - x1))
-                                .setScale(HelloController.getPre(), RoundingMode.HALF_UP).doubleValue())
-                        .append("\n");
-                this.stringBuilder.append("\n------------------------------------------------\n\n");
-
-                // updating the values of x0 and x1
-                x0 = x1;
-                x1 = x2;
-                iterations++;
-            } while (Math.abs((x1 - x0) / x1) * 100 >= conv_level && iterations <= itr - 1);
-
-            double xr = BigDecimal.valueOf(x1)
+            x2 = BigDecimal.valueOf(x1 - (expression(x1) * (x0 - x1)) / (expression(x0) - expression(x1)))
                     .setScale(HelloController.getPre(), RoundingMode.HALF_UP).doubleValue();
-            this.stringBuilder.append("The Final root of the equation =").append(xr).append("\n")
-                    .append("The number of iterations: ").append(iterations);
 
-            this.finalAnswer.append("The value of the final root = ").append(xr).append("\n")
-                    .append("The number of iterations: ").append(iterations);
+            // The Steps
+            this.stringBuilder.append("Iteration #").append(iterations + 1).append(": Xi-1= ")
+                    .append(x0).append(", xi= ").append(x1).append(", xi+1= ").append(x2)
+                    .append("\nRelative Approximate Error= ").append(BigDecimal.valueOf(Math.abs(x2 - x1))
+                            .setScale(HelloController.getPre(), RoundingMode.HALF_UP).doubleValue())
+                    .append("\n");
+            this.stringBuilder.append("\n------------------------------------------------\n\n");
 
-            // For the plot
-            this.values.add(xr);
+            // updating the values of x0 and x1
+            x0 = x1;
+            x1 = x2;
+            iterations++;
+        } while (Math.abs((x1 - x0) / x1) * 100 >= conv_level && iterations <= itr - 1);
 
-        } else {
-            this.stringBuilder.append("In this interval there is no root :))");
-        }
+        double xr = BigDecimal.valueOf(x1)
+                .setScale(HelloController.getPre(), RoundingMode.HALF_UP).doubleValue();
+        this.stringBuilder.append("The Final root of the equation =").append(xr).append("\n")
+                .append("The number of iterations: ").append(iterations);
+
+        this.finalAnswer.append("The value of the final root = ").append(xr).append("\n")
+                .append("The number of iterations: ").append(iterations);
+
+        // For the plot
+        this.values.add(xr);
     }
 
     public List<Double> getValues() {
